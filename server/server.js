d8c3c0b40e235d39866d8b7ba42a02297bfd26a6
Kaushal Kishor Rauniyar <157037723+kaushalxluciferr@users.noreply.github.com>
2025-01-11 10:36:10 +0530

    push again

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import cors from 'cors'
-import 'dotenv/config'
+import dotenv from 'dotenv';
+dotenv.config();
 import './config/instrument.js'
 import * as Sentry from '@sentry/node'
 
